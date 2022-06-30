ed87b4ee2974453e7f442c66b1549b5b00b22eaf
raisa-design <almeida.alves.raisa@gmail.com>
2022-06-30 20:50:55 -0300

    feat:change hopage

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Olá, mundo!
+          Somos os melhores devs
         </p>
         <a
           className="App-link"
